6c4beea46ee4dd320e003d1d3f0af5f0eca6a80f
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-17 11:09:09 -0700

    Initial Commit

---
+++ Graphicsl-In-SwiftUI-Animation/Graphicsl_In_SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Graphicsl_In_SwiftUI_AnimationApp.swift
+//  Graphicsl-In-SwiftUI-Animation
+//
+//  Created by Mitya Kim on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Graphicsl_In_SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
